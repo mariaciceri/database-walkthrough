704ce69eff49f247345793c5b449b9c599b65e50
Maria <mariafacchin00@hotmail.com>
2024-12-10 12:59:24 +0100

    Initialize date and form select

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -5,3 +5,15 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('.modal').modal();
   });
+
+$(document).ready(function(){
+  $('.datepicker').datepicker({
+    format: 'yyyy-mm-dd',
+    i18n: {done: 'Select'},
+    autoClose: true
+  });
+});
+
+$(document).ready(function(){
+  $('select').formSelect();
+});
