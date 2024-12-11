f8882edab3cc115a198bf13806b870a47365c1ba
Maria <mariafacchin00@hotmail.com>
2024-12-11 16:32:51 +0100

    Fix code to deployment

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -7,8 +7,16 @@ if os.path.exists("env.py"): # will only import the env.py file if it exists. (T
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
 
 db = SQLAlchemy(app)
+
 
 from taskmanager import routes
